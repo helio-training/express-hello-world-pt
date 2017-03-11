72d01d67a916043bd90fa1f5ce5edaf8771cfeb3
Tyler Garlick <tjgarlick@gmail.com>
2017-03-11 13:31:53 -0700

    Work on routes and public folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import Express from 'express';
+import BodyParser from 'body-parser';
 
 const app = Express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.use(BodyParser.json());
+app.use(Express.static('public'));
+
+
+app.get('/greeting', (req, res) => {
+  res.json({ greeting: 'Hello, Tyler'});
 });
+
+app.get('/greeting/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ greeting: `Hello, ${name}`});
+});
+
+app.post('/people', (req, res) => {
+  const person = req.body;
+  console.log(person);
+  return res.send(person);
+});
+
+
 
 app.listen(3000, () => console.log(`App started at: http://localhost:3000`));
